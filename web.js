360ce5dbcd699876b13e5d9d65e1aad4e86385ce
Pepo Saqer <peposakr23@gmail.com>
2022-01-10 17:39:59 +0200

    fixed error

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ $(".home-icons ul li a").hover(function(){
     $(this).children("span").show(500);
     $(this).children("i").css("background-color" , "#ffb421");
     $(this).css("background-color" , "#ffb421");
+    $(this).css("padding-left" , "0.6rem");
     }, function(){
     $(this).children("span").hide(500);
     $(this).children("i").css("background-color" , "#2b2a2a");
     $(this).css("background-color" , "#2b2a2a");
+    $(this).css("padding-left" , "0rem");
 });
 
 
@@ -18,3 +20,25 @@ $(".plog").hover(function(){
     $(this).children("div").hide(500);
     // $(this).css("background-image", 'url("img/WhatsApp\ Image\ 2021-11-26\ at\ 12.41.33\ PM.jpeg")');
 });
+
+
+const links = document.querySelectorAll('.user i');
+const sections = document.querySelectorAll('.forJS');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+  links.forEach((link) => link.classList.remove('active'));
+
+  // add the active class if within visible height of the element
+  if (scrollY - sections[index].offsetHeight <
+        sections[index].offsetTop) {
+    links[index].classList.add('active');
+  }
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
+
